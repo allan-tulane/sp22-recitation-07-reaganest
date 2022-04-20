8c379a6715c28b7c566df455b667c2d137c9b270
Reagan Esteves <89606813+reaganest@users.noreply.github.com>
2022-04-20 23:27:23 +0000

    finished

--- answers.md
+++ answers.md
@@ -1,26 +1,25 @@
 # CMPS 2200 Recitation 7
 ## Answers
 
-**Name:**_________________________
-
+**Name:** Reagan Esteves and Chenyu Zhao
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
+- **d.** Do you see a consistent trend? If so, what is it?
 
+|File        | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length|
+|------------|---------------------|----------------|-------------------------|
+|f1.txt      |         1340        |       826      |       0.616417910       |
+|alice29.txt |        1039367      |      676374    |       0.650755700       |
+|asyoulik.txt|        876253       |      606448    |       0.692092352       |
+|grammar.lsp |         26047       |      17356     |       0.666333935       |
+|fields.c    |         78050       |      56206     |       0.720128123       |
 
-- **d.**
-
-File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
-----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
-
-
-
+Based on the table, there is an consitent trend. The trend is the huffman coding is always less than fixed-length coding which means huffman coding. Moreover, the huffman vs fixed-length values are all shows that the huffman coding is about 2/3 the size of the fixed-length coding.
 
 - **e.**
+
+If every character had the same frequency, expected cost of the huffman encoding for the document would be the same.Moreover, the cost of the huffman coding would also be about the same as the fixed length coding in that specific scenario. The cost of the document is based on the frequency of every letter. Therefore, it is consistent across all the documents if each character in all the documents had the same frequency.
+
 
 
--- main.py
+++ main.py
@@ -2,60 +2,123 @@ import math, queue
 from collections import Counter
 
 class TreeNode(object):
-    # we assume data is a tuple (frequency, character)
     def __init__(self, left=None, right=None, data=None):
         self.left = left
         self.right = right
         self.data = data
+
     def __lt__(self, other):
-        return(self.data < other.data)
+        return (self.data < other.data)
+
     def children(self):
-        return((self.left, self.right))
-    
+        return ((self.left, self.right))
+
+
 def get_frequencies(fname):
-    ## This function is done.
-    ## Given any file name, this function reads line by line to count the frequency per character. 
-    f=open(fname, 'r')
+    f = open(fname, 'r')
     C = Counter()
     for l in f.readlines():
         C.update(Counter(l))
-    return(dict(C.most_common()))
+    return (dict(C.most_common()))
 
-# given a dictionary f mapping characters to frequencies, 
-# create a prefix code tree using Huffman's algorithm
+#TODO
 def make_huffman_tree(f):
     p = queue.PriorityQueue()
-    # construct heap from frequencies, the initial items should be
-    # the leaves of the final tree
     for c in f.keys():
-        p.put(TreeNode(None,None,(f[c], c)))
-
-    # greedily remove the two nodes x and y with lowest frequency,
-    # create a new node z with x and y as children,
-    # insert z into the priority queue (using an empty character "")
+        p.put(TreeNode(None, None, (f[c], c)))
     while (p.qsize() > 1):
-        # TODO
-        
+        a = p.get()
+        b = p.get()
+        c = a.data[0] + b.data[0]
+        p.put(TreeNode(a, b, (c, "")))
     # return root of the tree
     return p.get()
 
-# perform a traversal on the prefix code tree to collect all encodings
+  
+#TODO
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+  
+  if node.left is not None:
+    pre_l = prefix + '0'
+    get_code(node.left, pre_l)
+    
+  if node.right is not None:
+    pre_r = prefix + '1'
+    get_code(node.right, pre_r)
 
-# given an alphabet and frequencies, compute the cost of a fixed length encoding
+  if node.right is None and node.left is None:
+    code[node.data[1]] = prefix
+  return code
+
+
+#TODO
 def fixed_length_cost(f):
-    # TODO
-    pass
+    a = get_frequencies(f)
+    len_a = len(a.keys())
+    c_char = math.ceil(math.log2(len_a))
+    return c_char * sum(a.values())
 
-# given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
+  
+#TODO
 def huffman_cost(C, f):
-    # TODO
-    pass
+    h_cost = 0
+    for character in f:
+      b_len = len(C[character])
+      freq = f[character] 
+      
+      h_cost = h_cost + (b_len * freq)
+    return h_cost
 
-f = get_frequencies('f1.txt')
-print("Fixed-length cost:  %d" % fixed_length_cost(f))
+#alice29.txt
+f = get_frequencies('alice29.txt')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('alice29.txt'))
 T = make_huffman_tree(f)
 C = get_code(T)
+print(C)
 print("Huffman cost:  %d" % huffman_cost(C, f))
+
+
+"""
+#asyoulik.txt
+f = get_frequencies('asyoulik.txt')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('asyoulik.txt'))
+T = make_huffman_tree(f)
+C = get_code(T)
+print(C)
+print("Huffman cost:  %d" % huffman_cost(C, f))
+"""
+
+"""
+#f1.txt
+f = get_frequencies('f1.txt')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('f1.txt'))
+T = make_huffman_tree(f)
+C = get_code(T)
+print(C)
+print("Huffman cost:  %d" % huffman_cost(C, f))
+"""
+
+"""
+#grammar.lsp
+f = get_frequencies('grammar.lsp')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('grammar.lsp'))
+T = make_huffman_tree(f)
+C = get_code(T)
+print(C)
+print("Huffman cost:  %d" % huffman_cost(C, f))
+"""
+
+"""
+#fields.c
+f = get_frequencies('fields.c')
+print(f)
+print("Fixed-length cost:  %d" % fixed_length_cost('fields.c'))
+T = make_huffman_tree(f)
+C = get_code(T)
+print(C)
+print("Huffman cost:  %d" % huffman_cost(C, f))
+"""
